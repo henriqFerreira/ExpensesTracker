a58115636d7a1e28e19b3c920b89b905eb5c0ce1
henriqFerreira <henriquepfn@gmail.com>
2023-01-11 23:17:48 -0300

    Telas de Sign-In e Sign-Up

---
+++ ExpensesTracker.API/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpensesTracker.API.Controllers
+{
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        [HttpGet("Account/SignIn")]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpGet("Account/SignUp")]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
