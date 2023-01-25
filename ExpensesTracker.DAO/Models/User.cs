b6c99c91bccec206d4e9ba850bb6022c4e14e834
henriqFerreira <henriquepfn@gmail.com>
2023-01-25 11:34:10 -0300

    Adicionando DataAnnotations nos models e realizando Migration.

--- ExpensesTracker.DAO/Models/User.cs
+++ ExpensesTracker.DAO/Models/User.cs
@@ -1,11 +1,25 @@
-﻿namespace ExpensesTracker.DAO.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpensesTracker.DAO.Models
 {
     public class User
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo 'FirstName' é obrigatório.")]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "O campo 'FirstName' deve ter entre 1 e 100 caracteres.")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "O campo 'LastName' é obrigatório.")]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "O campo 'LastName' deve ter entre 1 e 100 caracteres.")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "O campo 'Email' é obrigatório.")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo 'Password' é obrigatório.")]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "O campo 'Password' deve ter entre 1 e 100 caracteres..")]
         public string Password { get; set; }
     }
 }
